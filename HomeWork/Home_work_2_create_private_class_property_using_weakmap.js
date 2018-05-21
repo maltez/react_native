c4d5386e40b524f79db193ec4d2db8fbc5f99452
Mikhail Kucherenko <mikhail.kucherenko@dish.com>
2018-05-21 18:52:59 +0300

    Homework2

--- HomeWork/Home_work_2_create_private_class_property_using_weakmap.js
+++ HomeWork/Home_work_2_create_private_class_property_using_weakmap.js
@@ -1,3 +1,4 @@
+//Task 1 #############################
 let Car = (function () {
     let privateProperties = new WeakMap();
   
@@ -26,3 +27,35 @@ let Car = (function () {
   myCar.showCar();
   myCar.changeCarColor("Black");
   myCar.showCar();
+
+  //Task 2 #############################
+
+  class FibonacciNumbers {
+    constructor(num) {
+        this.num = num;
+    }
+    [Symbol.iterator]() {
+        let a = 1, b = 1;
+        let c;
+        let num = this.num;
+        let value;
+        return {
+            next() {
+                c = a + b;
+                a = b;
+                b = c;
+                value = b;
+                //console.log("this.num = " + num);
+                return {
+                    value, done: b >= num
+                };
+            }
+        }
+    } 
+}
+
+const fib = new FibonacciNumbers(7);
+
+for (let i of fib) {
+    console.log(i);
+}
