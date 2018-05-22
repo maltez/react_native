baa1bf6ff52895a1eeb74111c3ed5d9c6609c626
vitaliisysoiev <39300963+vitaliisysoiev@users.noreply.github.com>
2018-05-22 12:36:10 +0300

    Update hw1_vitalii.s.js

--- HomeWork/vitalii.s/hw1_vitalii.s.js
+++ HomeWork/vitalii.s/hw1_vitalii.s.js
@@ -1,7 +1,8 @@
+const s_property = Symbol("property");
+const s_method = Symbol("method");
+
 class HomeWork1 {
     constructor(property) {
-        const s_property = Symbol("property");
-        const s_method = Symbol("method");
         this[s_property] = property;
     }
     [s_method]() {
@@ -15,4 +16,4 @@ class HomeWork1 {
 
 let hw1 = new HomeWork1("new property");
 hw1.run();
-console.log(hw1[s_property]);
+console.log(hw1[s_property]);//?
