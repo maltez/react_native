1aa18ea8a275838720e06089199bd1b04e6cdc66
Nick Lototskiy <nick.lototskiy@namecheap.com>
2018-06-11 07:56:32 +0300

    Add new presentations

---
+++ Examples/07-React16/src/samples/01.error.handling/app.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { MyErrorBoundary } from './error.boundary';
+import { Profile } from './profile.component';
+  
+  export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: { name: 'Nick' }
+    };
+  }
+  
+  updateUser = () => {
+    this.setState(state => ({ ...state, user: null }));
+  };
+  
+  render() {
+    return (
+      <div>
+        <MyErrorBoundary>
+          <Profile user={this.state.user} />
+          <button onClick={this.updateUser}>Update</button>
+        </MyErrorBoundary>
+      </div>
+    );
+  }
+  }
---
+++ Examples/07-React16/src/samples/02.multiple.elements/fruit.component.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+export const Fruits = () => [
+    <li key="1">Apple</li>,
+    <li key="2">Orange</li>,
+    <li key="3">Banana</li>
+  ];
