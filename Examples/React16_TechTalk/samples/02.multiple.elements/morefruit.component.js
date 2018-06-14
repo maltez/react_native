080811a3dfd612449b17dab9074fcd36c944f6a9
Nick Lototskiy <nicklototskiy@MacBook-Air-Nick.local>
2018-06-14 06:49:36 +0300

    Fix conflicts

--- Examples/React16_TechTalk/samples/02.multiple.elements/morefruit.component.js
+++
@@ -1,6 +0,0 @@
-import React, { Component } from 'react';
-export class MoreFruits extends Component {
-    render() {
-      return [<li key="1">Strawberry</li>, <li key="2">Blueberry</li>];
-    }
-  }
