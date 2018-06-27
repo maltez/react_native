effd865f0bc19dd02e49e34011c03c44ef7cf7c5
Iurii Khvorost <iurii.khvorost@gmail.com>
2018-06-27 16:22:27 +0300

    Homework 11

---
+++ HomeWork/iurii.khvorost/homework-11/Contact/contactStyles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native"
+
+export default ContactStyle = StyleSheet.create({
+    container: { 
+        paddingTop: 20,
+        flexDirection: "row"
+    },
+    details: { flex: 1, paddingLeft: 15 },
+    image: { width: 64, height: 64 },
+    email: { },
+    subject: { fontWeight: "bold", paddingTop: 3 },
+    text: { color: 'gray' }
+});
